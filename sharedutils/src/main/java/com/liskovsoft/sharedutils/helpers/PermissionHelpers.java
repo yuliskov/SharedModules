0c62518322630431399d77355d7100d140fa483a
Yuriy Liskov <firsthash@gmail.com>
2020-03-25 18:54:03 +0200

    upd

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/PermissionHelpers.java
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/PermissionHelpers.java
@@ -5,17 +5,22 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.os.Build;
+import android.os.Build.VERSION;
 import androidx.core.app.ActivityCompat;
 
 @TargetApi(16)
 public class PermissionHelpers {
     // Storage Permissions
     public static final int REQUEST_EXTERNAL_STORAGE = 112;
-
     private static String[] PERMISSIONS_STORAGE = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    // Mic Permissions
+    public static final int REQUEST_MIC = 113;
+    private static String[] PERMISSIONS_MIC = {
+            Manifest.permission.RECORD_AUDIO
     };
 
     /**
@@ -27,23 +32,11 @@ public class PermissionHelpers {
      * @param activity to apply permissions to
      */
     public static void verifyStoragePermissions(Context activity) {
-        if (!hasStoragePermissions(activity) && activity instanceof Activity && !Helpers.isGenymotion()) {
-            requestStoragePermissions((Activity) activity);
-        }
+        requestPermissions(activity, PERMISSIONS_STORAGE, REQUEST_EXTERNAL_STORAGE);
     }
 
-    /**
-     * Shows permissions dialog<br/>
-     * NOTE: runs async
-     * @param activity context
-     */
-    private static void requestStoragePermissions(Activity activity) {
-        // We don't have permission so prompt the user
-        ActivityCompat.requestPermissions(
-                activity,
-                PERMISSIONS_STORAGE,
-                REQUEST_EXTERNAL_STORAGE
-        );
+    public static void verifyMicPermissions(Context activity) {
+        requestPermissions(activity, PERMISSIONS_MIC, REQUEST_MIC);
     }
 
     /**
@@ -52,12 +45,32 @@ public class PermissionHelpers {
      * @return whether permission already granted
      */
     public static boolean hasStoragePermissions(Context activity) {
-        if (Build.VERSION.SDK_INT >= 23) {
-            // Check if we have write permission
-            return hasPermissions(activity, PERMISSIONS_STORAGE);
-        }
+        // Check if we have write permission
+        return hasPermissions(activity, PERMISSIONS_STORAGE);
+    }
 
-        return true;
+    public static boolean hasMicPermissions(Context activity) {
+        // Check if we have mic permission
+        return hasPermissions(activity, PERMISSIONS_MIC);
+    }
+
+    // Utils
+
+    /**
+     * Shows permissions dialog<br/>
+     * NOTE: runs async
+     */
+    private static void requestPermissions(Context activity, String[] permissions, int requestId) {
+        if (!hasPermissions(activity, permissions) && !Helpers.isGenymotion()) {
+            if (activity instanceof Activity) {
+                // We don't have permission so prompt the user
+                ActivityCompat.requestPermissions(
+                        (Activity) activity,
+                        permissions,
+                        requestId
+                );
+            }
+        }
     }
 
     /**
@@ -66,10 +79,12 @@ public class PermissionHelpers {
      * @return whether permission already granted
      */
     private static boolean hasPermissions(Context activity, String... permissions) {
-        for (String permission : permissions) {
-            int result = ActivityCompat.checkSelfPermission(activity, permission);
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                return false;
+        if (VERSION.SDK_INT >= 23) {
+            for (String permission : permissions) {
+                int result = ActivityCompat.checkSelfPermission(activity, permission);
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
             }
         }
 
