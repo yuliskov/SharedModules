de3dce41f1b617dfc77d405a9b95d2348e7cdd69
Yuriy Liskov <firsthash@gmail.com>
2021-02-18 09:15:21 +0200

    refactor

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/locale/LocaleContextWrapper.java
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/locale/LocaleContextWrapper.java
@@ -1,22 +1,23 @@
 package com.liskovsoft.sharedutils.locale;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.res.Configuration;
 import android.content.res.Resources;
-import android.os.Build;
 import android.os.Build.VERSION;
 import android.os.LocaleList;
 
 import java.util.Locale;
 
+/**
+ * <a href="https://stackoverflow.com/questions/4985805/set-locale-programmatically">More info</a>
+ */
 public class LocaleContextWrapper extends ContextWrapper {
     public LocaleContextWrapper(Context base) {
         super(base);
     }
 
-    @TargetApi(Build.VERSION_CODES.N)
+    @SuppressWarnings("deprecation")
     public static Context wrap(Context context, Locale newLocale) {
         if (newLocale == null) {
             return context;
@@ -49,5 +50,23 @@ public class LocaleContextWrapper extends ContextWrapper {
         }
 
         return new ContextWrapper(context);
+    }
+
+    @SuppressWarnings("deprecation")
+    public static void apply(Configuration configuration, Locale newLocale) {
+        if (newLocale == null || configuration == null) {
+            return;
+        }
+
+        if (VERSION.SDK_INT >= 24) {
+            configuration.setLocale(newLocale);
+            LocaleList localeList = new LocaleList(newLocale);
+            LocaleList.setDefault(localeList);
+            configuration.setLocales(localeList);
+        } else if (VERSION.SDK_INT >= 17) {
+            configuration.setLocale(newLocale);
+        } else {
+            configuration.locale = newLocale;
+        }
     }
 }
