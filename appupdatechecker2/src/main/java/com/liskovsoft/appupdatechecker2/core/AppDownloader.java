b61935b3823d662a332b1c3ed02a894ae8eb531b
Yuriy Liskov <firsthash@gmail.com>
2021-10-05 08:14:09 +0300

    AppDownloader refactor and exception fix

--- appupdatechecker2/src/main/java/com/liskovsoft/appupdatechecker2/core/AppDownloader.java
+++ appupdatechecker2/src/main/java/com/liskovsoft/appupdatechecker2/core/AppDownloader.java
@@ -18,12 +18,13 @@ import java.io.File;
  *   downloader.download(new String[]{"http://serverurl/appfile.apk"});
  * </pre>
  */
-public class AppDownloader extends AsyncTask<Uri[],Void,Void> {
+public class AppDownloader {
     private static final String TAG = AppDownloader.class.getSimpleName();
     private static final int MIN_APK_SIZE_BYTES = 15_000_000;
     private final Context mContext;
     private boolean mInProgress;
     private final AppDownloaderListener mListener;
+    private AppDownloadTask mDownloadTask;
 
     public AppDownloader(Context context, AppDownloaderListener listener) {
         mContext = context;
@@ -35,71 +36,82 @@ public class AppDownloader extends AsyncTask<Uri[],Void,Void> {
      */
     public void download(Uri[] downloadUris) {
         if (!mInProgress) {
-            execute(downloadUris);
+            if (mDownloadTask == null) {
+                mDownloadTask = new AppDownloadTask();
+                mDownloadTask.execute(downloadUris);
+            } else {
+                Log.e(TAG, "DownloadTask not null. Strange...");
+            }
         } else {
             Log.e(TAG, "Another downloading in progress. Canceling...");
         }
     }
 
-    @Override
-    protected Void doInBackground(Uri[]... args) {
-        mInProgress = true;
+    private class AppDownloadTask extends AsyncTask<Uri[],Void,String> {
+        @Override
+        protected String doInBackground(Uri[]... args) {
+            mInProgress = true;
 
-        Uri[] uris = args[0];
+            Uri[] uris = args[0];
 
-        String path = null;
-        for (Uri uri : uris) {
-            if (URLUtil.isValidUrl(uri.toString())) {
-                path = downloadPackage(uri.toString());
-                if (path != null)
-                    break;
-            }
-        }
-
-        if (path != null) {
-            mListener.onApkDownloaded(path);
-        } else {
-            String msg = "Error while download. Install path is null";
-            Log.e(TAG, msg);
-            mListener.onDownloadError(new IllegalStateException(msg));
-        }
-
-        mInProgress = false;
-
-        return null;
-    }
-
-    private String downloadPackage(String uri) {
-        File cacheDir = FileHelpers.getCacheDir(mContext);
-        if (cacheDir == null) {
-            return null;
-        }
-        File outputFile = new File(cacheDir, "update.apk");
-        String path = null;
-        try {
-            DownloadManager manager = new DownloadManager(mContext);
-            MyRequest request = new MyRequest(Uri.parse(uri));
-            request.setDestinationUri(Uri.fromFile(outputFile));
-            try {
-                long id = manager.enqueue(request);
-                int size = manager.getSizeForDownloadedFile(id);
-                Uri destination = manager.getUriForDownloadedFile(id);
-
-                if (destination != null) {
-                    // It could be a web page instead of apk
-                    if (size > MIN_APK_SIZE_BYTES) {
-                        path = destination.getPath();
-                    } else { // do cleanup
-                        FileHelpers.delete(destination.getPath());
-                    }
+            String path = null;
+            for (Uri uri : uris) {
+                if (URLUtil.isValidUrl(uri.toString())) {
+                    path = downloadPackage(uri.toString());
+                    if (path != null)
+                        break;
                 }
-            } catch (IllegalStateException ex) { // 403 or something else
-                Log.d(TAG, ex.toString());
             }
-        } catch (IllegalStateException ex) { // CANNOT OBTAIN WRITE PERMISSIONS
-            Log.e(TAG, ex.getMessage(), ex);
+
+            return path;
         }
-        return path;
+
+        @Override
+        protected void onPostExecute(String path) {
+            if (path != null) {
+                mListener.onApkDownloaded(path);
+            } else {
+                String msg = "Error while download. Install path is null";
+                Log.e(TAG, msg);
+                mListener.onDownloadError(new IllegalStateException(msg));
+            }
+
+            mInProgress = false;
+            mDownloadTask = null;
+        }
+
+        private String downloadPackage(String uri) {
+            File cacheDir = FileHelpers.getCacheDir(mContext);
+            if (cacheDir == null) {
+                return null;
+            }
+            File outputFile = new File(cacheDir, "update.apk");
+            String path = null;
+            try {
+                DownloadManager manager = new DownloadManager(mContext);
+                MyRequest request = new MyRequest(Uri.parse(uri));
+                request.setDestinationUri(Uri.fromFile(outputFile));
+                try {
+                    long id = manager.enqueue(request);
+                    int size = manager.getSizeForDownloadedFile(id);
+                    Uri destination = manager.getUriForDownloadedFile(id);
+
+                    if (destination != null) {
+                        // It could be a web page instead of apk
+                        if (size > MIN_APK_SIZE_BYTES) {
+                            path = destination.getPath();
+                        } else { // do cleanup
+                            FileHelpers.delete(destination.getPath());
+                        }
+                    }
+                } catch (IllegalStateException ex) { // 403 or something else
+                    Log.d(TAG, ex.toString());
+                }
+            } catch (IllegalStateException ex) { // CANNOT OBTAIN WRITE PERMISSIONS
+                Log.e(TAG, ex.getMessage(), ex);
+            }
+            return path;
+        }
     }
 
     public boolean isInProgress() {
