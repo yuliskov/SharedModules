63a4f999daaaf1e890bce254af8a37eda97ca280
Yuriy Liskov <firsthash@gmail.com>
2021-12-16 18:05:22 +0200

    cipher fix

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/okhttp/OkHttpCommons.java
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/okhttp/OkHttpCommons.java
@@ -39,8 +39,9 @@ public final class OkHttpCommons {
             CipherSuite.TLS_AES_256_GCM_SHA384,
             CipherSuite.TLS_CHACHA20_POLY1305_SHA256,
             CipherSuite.TLS_AES_128_CCM_SHA256,
-            // Robolectric mocking error
+            // Robolectric error (no such field). Constructing manually.
             //CipherSuite.TLS_AES_256_CCM_8_SHA256,
+            CipherSuite.forJavaName("TLS_AES_256_CCM_8_SHA256"),
 
             CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
             CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
