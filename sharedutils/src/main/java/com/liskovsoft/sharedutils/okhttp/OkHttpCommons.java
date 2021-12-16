1768cd2755f99c93bbc97c03e8e8ea652346523c
Yuriy Liskov <firsthash@gmail.com>
2021-12-16 17:04:31 +0200

    add retromock + Robolectric mocking errorfix

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/okhttp/OkHttpCommons.java
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/okhttp/OkHttpCommons.java
@@ -39,7 +39,8 @@ public final class OkHttpCommons {
             CipherSuite.TLS_AES_256_GCM_SHA384,
             CipherSuite.TLS_CHACHA20_POLY1305_SHA256,
             CipherSuite.TLS_AES_128_CCM_SHA256,
-            CipherSuite.TLS_AES_256_CCM_8_SHA256,
+            // Robolectric mocking error
+            //CipherSuite.TLS_AES_256_CCM_8_SHA256,
 
             CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
             CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
