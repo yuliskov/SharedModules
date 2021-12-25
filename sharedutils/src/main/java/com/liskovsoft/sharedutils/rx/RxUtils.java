31d90a666eea6ab2c9b8a0ef7eda22579cc35859
Yuriy Liskov <firsthash@gmail.com>
2021-12-25 23:52:44 +0200

    rx utils

---
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/rx/RxUtils.java
@@ -0,0 +1,62 @@
+package com.liskovsoft.sharedutils.rx;
+
+import com.liskovsoft.sharedutils.mylogger.Log;
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+public class RxUtils {
+    private static final String TAG = RxUtils.class.getSimpleName();
+
+    public static void disposeActions(Disposable... actions) {
+        if (actions != null) {
+            for (Disposable action : actions) {
+                boolean updateInProgress = action != null && !action.isDisposed();
+
+                if (updateInProgress) {
+                    action.dispose();
+                }
+            }
+        }
+    }
+
+    public static <T> Disposable execute(Observable<T> observable) {
+        return observable
+                .subscribeOn(Schedulers.newThread())
+                .subscribe(
+                        obj -> {}, // ignore result
+                        error -> Log.e(TAG, "Execute error: %s", error.getMessage())
+                );
+    }
+
+    /**
+     * NOTE: Don't use it to check that action in completed inside other action (scrollEnd bug).
+     */
+    public static boolean isAnyActionRunning(Disposable... actions) {
+        if (actions != null) {
+            for (Disposable action : actions) {
+                if (action != null && !action.isDisposed()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static Disposable startInterval(Runnable callback, int periodSec) {
+        Observable<Long> playbackProgressObservable =
+                Observable.interval(periodSec, TimeUnit.SECONDS);
+
+        return playbackProgressObservable
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(
+                        period -> callback.run(),
+                        error -> Log.e(TAG, "startInterval error: %s", error.getMessage())
+                );
+    }
+}
