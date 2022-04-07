ec19c50d6fe75ded2e0d4bf24fe13c68354fb815
Yuriy Liskov <firsthash@gmail.com>
2022-04-07 17:01:55 +0300

    provider: storage path not found fix

--- appupdatechecker2/src/main/java/com/liskovsoft/appupdatechecker2/other/GenericFileProvider.java
+++ appupdatechecker2/src/main/java/com/liskovsoft/appupdatechecker2/other/GenericFileProvider.java
@@ -1,6 +1,10 @@
 package com.liskovsoft.appupdatechecker2.other;
 
+import android.content.Context;
 import androidx.core.content.FileProvider;
 
+/**
+ * Usage: {@link com.liskovsoft.sharedutils.helpers.FileHelpers#getFileUri(Context, String)}
+ */
 public class GenericFileProvider extends FileProvider {
 }
