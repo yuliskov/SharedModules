192f5d333ab2cef321f36456d26b23624efa97be
Yuriy Liskov <firsthash@gmail.com>
2022-12-11 15:48:44 +0200

    rx utils: add global ex handler

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/rx/RxUtils.java
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/rx/RxUtils.java
@@ -6,8 +6,12 @@ import io.reactivex.Completable;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.exceptions.UndeliverableException;
+import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
 
+import java.io.IOException;
+import java.net.SocketException;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -152,5 +156,38 @@ public class RxUtils {
                             }
                         }
                 );
+    }
+
+    /**
+     * <a href="https://stackoverflow.com/questions/43525052/rxjava2-observable-take-throws-undeliverableexception">More info 1</a>
+     * <a href="https://github.com/ReactiveX/RxJava/wiki/What's-different-in-2.0#error-handling">More info 2</a>
+     */
+    public static void setupGlobalErrorHandler() {
+        RxJavaPlugins.setErrorHandler(e -> {
+            if (e instanceof UndeliverableException) {
+                e = e.getCause();
+            }
+            if (e instanceof IOException) {
+                // fine, irrelevant network problem or API that throws on cancellation
+                return;
+            }
+            if (e instanceof InterruptedException) {
+                // fine, some blocking code was interrupted by a dispose call
+                return;
+            }
+            if ((e instanceof NullPointerException) || (e instanceof IllegalArgumentException)) {
+                // that's likely a bug in the application
+                Thread.currentThread().getUncaughtExceptionHandler()
+                        .uncaughtException(Thread.currentThread(), e);
+                return;
+            }
+            if (e instanceof IllegalStateException) {
+                // that's a bug in RxJava or in a custom operator
+                Thread.currentThread().getUncaughtExceptionHandler()
+                        .uncaughtException(Thread.currentThread(), e);
+                return;
+            }
+            Log.e(TAG, "Undeliverable exception received, not sure what to do", e);
+        });
     }
 }
