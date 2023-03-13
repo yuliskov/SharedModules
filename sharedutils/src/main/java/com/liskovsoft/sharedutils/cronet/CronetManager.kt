a5ee919be7af1a5f9f331f59e08a5d8c6cae7e48
Yuriy Liskov <firsthash@gmail.com>
2023-03-13 19:13:55 +0200

    cronet: always use native impl

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/cronet/CronetManager.kt
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/cronet/CronetManager.kt
@@ -2,6 +2,7 @@ package com.liskovsoft.sharedutils.cronet
 
 import android.content.Context
 import org.chromium.net.CronetEngine
+import org.chromium.net.impl.NativeCronetProvider
 
 object CronetManager {
     private var engine: CronetEngine? = null
@@ -12,7 +13,14 @@ object CronetManager {
             //val wrapper = CronetEngineWrapper(context)
             //engine = wrapper.cronetEngine
 
-            engine = CronetEngine.Builder(context)
+            //engine = CronetEngine.Builder(context)
+            //    .enableQuic(true)
+            //    .enableHttp2(true)
+            //    .enableBrotli(true)
+            //    .build()
+
+            engine = NativeCronetProvider(context)
+                .createBuilder()
                 .enableQuic(true)
                 .enableHttp2(true)
                 .enableBrotli(true)
