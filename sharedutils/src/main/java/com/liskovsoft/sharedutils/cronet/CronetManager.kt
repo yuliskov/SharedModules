b2e3e3ef991bbc585fbe53db665b850cf895bd7a
Yuriy Liskov <firsthash@gmail.com>
2023-03-17 17:05:20 +0200

    cronet: exception fix

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/cronet/CronetManager.kt
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/cronet/CronetManager.kt
@@ -1,14 +1,16 @@
 package com.liskovsoft.sharedutils.cronet
 
 import android.content.Context
+import com.liskovsoft.sharedutils.mylogger.Log
 import org.chromium.net.CronetEngine
 import org.chromium.net.impl.NativeCronetProvider
 
 object CronetManager {
+    private val TAG = CronetManager::class.java.simpleName
     private var engine: CronetEngine? = null
 
     @JvmStatic
-    fun getEngine(context: Context): CronetEngine {
+    fun getEngine(context: Context): CronetEngine? {
         if (engine == null) {
             //val wrapper = CronetEngineWrapper(context)
             //engine = wrapper.cronetEngine
@@ -19,14 +21,21 @@ object CronetManager {
             //    .enableBrotli(true)
             //    .build()
 
-            engine = NativeCronetProvider(context)
-                .createBuilder()
-                .enableQuic(true)
-                .enableHttp2(true)
-                .enableBrotli(true)
-                .build()
+            try {
+                engine = NativeCronetProvider(context)
+                    .createBuilder()
+                    .enableQuic(true)
+                    .enableHttp2(true)
+                    .enableBrotli(true)
+                    .build()
+            } catch (e: UnsatisfiedLinkError) {
+                // Fatal Exception: java.lang.UnsatisfiedLinkError
+                // Cannot load library: soinfo_relocate(linker.cpp:982): cannot locate symbol "getauxval" referenced by "libcronet.101.0.4951.41.so".
+                e.printStackTrace()
+                Log.e(TAG, e.message)
+            }
         }
 
-        return engine!!
+        return engine
     }
 }
