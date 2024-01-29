b360e4ec23958230ca7f7c691fec0450ade8876d
Yuriy Liskov <firsthash@gmail.com>
2024-01-29 05:03:34 +0200

    Helpers: add fullscreen utils

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/Helpers.java
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/Helpers.java
@@ -36,6 +36,8 @@ import android.view.Display;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
 import android.view.WindowManager;
 import android.view.WindowManager.LayoutParams;
 import android.view.accessibility.AccessibilityNodeInfo;
@@ -547,6 +549,27 @@ public final class Helpers {
         } else {
             decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
         }
+    }
+
+    public static void makeActivityFullscreen2(Activity activity) {
+        if (Build.VERSION.SDK_INT >= 30) {
+            activity.getWindow().setDecorFitsSystemWindows(false);
+            WindowInsetsController controller = activity.getWindow().getInsetsController();
+            if (controller != null) {
+                controller.hide(WindowInsets.Type.systemBars());
+                controller.setSystemBarsBehavior(WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+            }
+        } else {
+            activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
+    public static void addFullscreenListener(Activity activity) {
+        activity.getWindow().getDecorView().setOnSystemUiVisibilityChangeListener(visibility -> {
+            if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                makeActivityFullscreen2(activity);
+            }
+        });
     }
 
     /**
