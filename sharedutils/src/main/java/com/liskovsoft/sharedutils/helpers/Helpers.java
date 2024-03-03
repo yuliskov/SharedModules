2d895e928ef112d6c389d2a01e2131dc9b2626d4
Yuriy Liskov <firsthash@gmail.com>
2024-03-03 04:54:28 +0200

    helpers upd

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/Helpers.java
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/Helpers.java
@@ -1406,8 +1406,15 @@ public final class Helpers {
         return outValue.resourceId;
     }
 
-    public static <T> T firstNonNull(T obj, T defObj) {
-        return obj != null ? obj : defObj;
+    @SafeVarargs
+    public static <T> T firstNonNull(T... objects) {
+        for (T obj : objects) {
+            if (obj != null) {
+                return obj;
+            }
+        }
+
+        return null;
     }
 
     public static int[] range(int start, int end, int step) {
