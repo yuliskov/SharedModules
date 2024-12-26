a37b72a2e4635f6bba148f5f77135e66074dfe2d
Yuriy Liskov <firsthash@gmail.com>
2024-12-26 23:01:44 +0200

    prefs upd

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/prefs/SharedPreferencesBase.java
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/prefs/SharedPreferencesBase.java
@@ -4,31 +4,69 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import androidx.preference.PreferenceManager;
 
+import com.liskovsoft.sharedutils.mylogger.Log;
+
+import java.io.File;
+
 public class SharedPreferencesBase {
+    private static final String TAG = SharedPreferencesBase.class.getSimpleName();
+    private static final long PREF_MAX_SIZE_MB = 5;
     private final SharedPreferences mPrefs;
     protected Context mContext;
 
     public SharedPreferencesBase(Context context, String prefName) {
-        mContext = context;
-        mPrefs = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
+        this(context, prefName, -1, false);
+    }
+
+    public SharedPreferencesBase(Context context, String prefName, boolean limitMaxSize) {
+        this(context, prefName, -1, limitMaxSize);
     }
 
     public SharedPreferencesBase(Context context, String prefName, int defValResId) {
-        mContext = context;
-        mPrefs = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
-        PreferenceManager.setDefaultValues(context, prefName, Context.MODE_PRIVATE, defValResId, true);
-    }
-
-    public SharedPreferencesBase(Context context, int defValResId) {
-        mContext = context;
-        mPrefs = PreferenceManager.getDefaultSharedPreferences(mContext);
-        String defaultPrefsName = context.getPackageName() + "_preferences";
-        PreferenceManager.setDefaultValues(context, defaultPrefsName, Context.MODE_PRIVATE, defValResId, true);
+        this(context, prefName, defValResId, false);
     }
 
     public SharedPreferencesBase(Context context) {
+        this(context, null, -1, false);
+    }
+
+    public SharedPreferencesBase(Context context, int defValResId) {
+        this(context, null, defValResId, false);
+    }
+
+    public SharedPreferencesBase(Context context, String prefName, int defValResId, boolean limitMaxSize) {
+        if (limitMaxSize) {
+            limitMaxSize(context, prefName);
+        }
+
         mContext = context;
-        mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        if (prefName != null) {
+            mPrefs = context.getSharedPreferences(prefName, Context.MODE_PRIVATE);
+        } else {
+            prefName = context.getPackageName() + "_preferences";
+            mPrefs = PreferenceManager.getDefaultSharedPreferences(context);
+        }
+
+        if (defValResId != -1) {
+            PreferenceManager.setDefaultValues(context, prefName, Context.MODE_PRIVATE, defValResId, true);
+        }
+    }
+
+    /**
+     * Delete prefs which size exceeds a limit to prevent unconditional behavior
+     */
+    private void limitMaxSize(Context context, String prefName) {
+        File sharedPrefs = new File(context.getApplicationInfo().dataDir, "shared_prefs" + "/" + prefName + ".xml");
+
+        if (sharedPrefs.exists() && sharedPrefs.isFile()) {
+            long sizeMB = sharedPrefs.length() / 1024 / 1024;
+
+            if (sizeMB > PREF_MAX_SIZE_MB) {
+                Log.e(TAG, "Shared preference max size exceeded. Deleting...");
+                sharedPrefs.delete();
+            }
+        }
     }
 
     public Context getContext() {
