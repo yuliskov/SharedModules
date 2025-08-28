aded71056e6f62e540534c3a6d036c34b5bda7a5
Yuriy Liskov <firsthash@gmail.com>
2025-08-28 22:43:32 +0300

    global intents refactor

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/GlobalConstants.java
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/helpers/GlobalConstants.java
@@ -1,7 +1,5 @@
 package com.liskovsoft.sharedutils.helpers;
 
 public class GlobalConstants {
-    public static final String RESTART_INTENT = "RESTART_INTENT";
     public static final String ATV_INTENT = "ATV_INTENT";
-    public static final String INCOGNITO_INTENT = "INCOGNITO_INTENT";
 }
