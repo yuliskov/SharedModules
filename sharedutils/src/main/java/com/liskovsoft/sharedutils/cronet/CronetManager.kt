4e708003364c2e2982ddd3ff1c0a6dcbcc45293d
Yuriy Liskov <firsthash@gmail.com>
2026-04-01 05:15:55 +0300

    CronetManager: remove unneeded tweaks

--- sharedutils/src/main/java/com/liskovsoft/sharedutils/cronet/CronetManager.kt
+++ sharedutils/src/main/java/com/liskovsoft/sharedutils/cronet/CronetManager.kt
@@ -31,33 +31,34 @@ object CronetManager {
                     .enableBrotli(true)
                     //.addQuicHint("youtube.com", 80, 80)
 
-                if (builder is ExperimentalCronetEngine.Builder) {
-                    val experimentalOptions = """
-                        {
-                            "AsyncResolver": {
-                                "enable_async_resolver": true
-                            },
-                            "StaleDNS": {
-                                "enable": true,
-                                "max_expired_time_ms": 86400000,
-                                "max_stale_uses": 5,
-                                "persist_to_disk": false,
-                                "use_stale_on_name_not_resolved": true
-                            },
-                            "QUIC": {
-                                "delay_tcp_race": true,
-                                "estimate_initial_rtt": true,
-                                "max_server_configs_stored_in_properties": 8,
-                                "idle_connection_timeout_seconds": 30,
-                                "goaway_sessions_on_ip_change": true,
-                                "initial_rtt_for_handshake_milliseconds": 100,
-                                "allow_other_network_reuse": true
-                            }
-                        }
-                    """.trimIndent()
-
-                    builder.setExperimentalOptions(experimentalOptions)
-                }
+                // Do these tweaks have negative side effects?
+                //if (builder is ExperimentalCronetEngine.Builder) {
+                //    val experimentalOptions = """
+                //        {
+                //            "AsyncResolver": {
+                //                "enable_async_resolver": true
+                //            },
+                //            "StaleDNS": {
+                //                "enable": true,
+                //                "max_expired_time_ms": 86400000,
+                //                "max_stale_uses": 5,
+                //                "persist_to_disk": false,
+                //                "use_stale_on_name_not_resolved": true
+                //            },
+                //            "QUIC": {
+                //                "delay_tcp_race": true,
+                //                "estimate_initial_rtt": true,
+                //                "max_server_configs_stored_in_properties": 8,
+                //                "idle_connection_timeout_seconds": 30,
+                //                "goaway_sessions_on_ip_change": true,
+                //                "initial_rtt_for_handshake_milliseconds": 100,
+                //                "allow_other_network_reuse": true
+                //            }
+                //        }
+                //    """.trimIndent()
+                //
+                //    builder.setExperimentalOptions(experimentalOptions)
+                //}
 
                 engine = builder.build()
             } catch (e: UnsatisfiedLinkError) {
